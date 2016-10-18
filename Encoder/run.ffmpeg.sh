f8129f95b198732549c1aba7aba2ee4105869783
Matthew Baggett <matthew@baggett.me>
2016-10-18 21:58:59 +0100

    It doesn't work.

--- Encoder/run.ffmpeg.sh
+++ Encoder/run.ffmpeg.sh
@@ -2,14 +2,10 @@
 
 while true; do
     echo "Configured as $CAMERA_NAME to connect to $CAMERA_SOURCE";
-    sleep 15;
+    sleep 30;
+    openRTSP -t $CAMERA_SOURCE | \
     ffmpeg \
-        -loglevel info \
-        -threads 4 \
-        $CAMERA_OPTIONS \
-        -i $CAMERA_SOURCE \
-        -framerate 25 \
-        -vcodec libvpx \
-        -c:a libopus \
-        http://service:8080/$CAMERA_NAME.ffm;
+        -i - \
+        -f webm \
+        -an http://service:8080/$CAMERA_NAME.ffm;
 done
