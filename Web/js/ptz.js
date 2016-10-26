a0e045e627e5ccd35f8fbd5dd752231539a01839
Wolfgang von Caron <wolfgang.voncaron@segura.co.uk>
2016-10-26 22:44:24 +0100

    Added wasd control for the cameras

--- Web/js/ptz.js
+++ Web/js/ptz.js
@@ -13,4 +13,43 @@ jQuery(document).ready(function(){
         });
 
     });
+
+    jQuery(document).keydown(function(e) {
+        var action = "";
+        switch(e.which) {
+            case 65: // a
+                action = "left";
+                break;
+
+            case 83: // s
+                action = "down";
+                break;
+
+            case 68: // d
+                action = "right";
+                break;
+
+            case 87: // w
+                action = "up";
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+
+        jQuery('input[type=checkbox].wasd-checkbox').each(function () {
+            if(this.checked){
+                var camera = jQuery(this).parents('.camera').attr('cameraname');
+
+                jQuery.ajax({
+                    url: "/camera/ptz",
+                    method: 'post',
+                    data: {
+                        action: action,
+                        camera: camera
+                    }
+                });
+
+            }
+        });
+    });
 });
